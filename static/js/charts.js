f16109e7b91181b6c94d50f9e5f91f301651b4d8
Chirag Maheshwari <csoni111@gmail.com>
2018-05-13 01:27:43 +0530

    add pie charts

--- static/js/charts.js
+++ static/js/charts.js
@@ -10,12 +10,46 @@ function visualize(allFs) {
     //     d["timestamp"] = dateFormat.parse(d["timestamp"]);
     // });
 
-
+    // create crossfilter instance
     const ndx = crossfilter(allFs);
 
+    // define data dimensions
+    const versionDim = ndx.dimension((d) => { return d.version; });
+    const archDim = ndx.dimension((d) => { return d.arch; });
+
+    // define data groups
+    const versionGroup = versionDim.group();
+    const archGroup = archDim.group();
     const all = ndx.groupAll();
 
+    // prepare charts
+    const versionPie = dc.pieChart("#version-pie");
+    const archPie = dc.pieChart("#arch-pie");
     const numberOfFS = dc.numberDisplay("#unique-fs-number");
+
+    versionPie
+        .slicesCap(4)
+        .innerRadius(35)
+        .dimension(versionDim)
+        .group(versionGroup)
+        .legend(dc.legend())
+        .on('pretransition', function(chart) {
+            chart.selectAll('text.pie-slice').text(function(d) {
+                return d.data.key + ' (' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%)';
+            })
+        });
+
+    archPie
+        .slicesCap(4)
+        .innerRadius(35)
+        .dimension(archDim)
+        .group(archGroup)
+        .legend(dc.legend())
+        .on('pretransition', function(chart) {
+            chart.selectAll('text.pie-slice').text(function(d) {
+                return d.data.key + ' (' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%)';
+            })
+        });
 
     numberOfFS
         .formatNumber(d3.format("d"))
